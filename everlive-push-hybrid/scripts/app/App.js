40cb8be1c61c8178f2d24984b1f70fc2fac9f99d
Nina Pratiwi <ferpect13@gmail.com>
2015-10-30 02:57:04 +0000

    Changes made by Nina Pratiwi (ferpect13@gmail.com).

--- everlive-push-hybrid/scripts/app/App.js
+++ everlive-push-hybrid/scripts/app/App.js
@@ -9,4 +9,28 @@ pushApp.controller('HomeCtrl', function ($scope) {
     {'name': 'MOTOROLA XOOM™',
      'snippet': 'The Next, Next Generation tablet.'}
   ];
+    
+     $scope.selectedstock = null;
+    
+      $scope.clientSideList = [
+    { text: "Backbone", value: "bb" },
+    { text: "Angular", value: "ng" },
+    { text: "Ember", value: "em" },
+    { text: "Knockout", value: "ko" }
+  ];
+
+  $scope.serverSideList = [
+    { text: "Go", value: "go" },
+    { text: "Python", value: "py" },
+    { text: "Ruby", value: "rb" },
+    { text: "Java", value: "jv" }
+  ];
+  
+  $scope.data = {
+    clientSide: 'ng'
+  };
+  
+  $scope.serverSideChange = function(item) {
+    console.log("Selected Serverside, text:", item.text, "value:", item.value);
+  };
 });
